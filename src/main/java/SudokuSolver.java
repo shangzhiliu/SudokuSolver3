eae3701ab75dc98ff770ca2d2d285c66e19ea796
Zhou-Yang-whatever <34389142+Zhou-Yang-whatever@users.noreply.github.com>
2019-02-01 16:42:06 -0800

    Update SudokuSolver.java

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -11,9 +11,22 @@ public class SudokuSolver {
 
     public static void main(String [] args) throws IOException {
         SudokuSolver solver = new SudokuSolver("src/test/resources/puzzle.sdk");
+        solver = solver.solve();
         System.out.println(solver.board);
-
+    
         SudokuSolver solver2 = new SudokuSolver("src/test/resources/puzzle.ss");
         System.out.println(solver2.board);
+    }
+    
+    public Board solve(Board board){
+        if(board.isSolved()){
+            return board;
+        }
+        
+        for(Board neighbor: board.getNeighbors()){
+            return solve(neighbor);
+        }
+        
+        return null;
     }
 }
